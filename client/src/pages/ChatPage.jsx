57764526840a68a9505d1aecccbc7ef7e943db02
Nitin <nitinnb31122002@gmail.com>
2023-11-26 12:35:44 +0530

    Final-2

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -41,7 +41,7 @@ const ChatPage = ({ username }) => {
     <div className="flex h-screen w-screen flex-col items-center justify-center bg-slate-200">
       <h1 className="mb-3 text-center text-3xl underline">Let's go chatters</h1>
       <div className="flex min-h-[70vh] min-w-[70vh] flex-col justify-between rounded-lg  bg-slate-100 px-2 py-2 shadow-xl shadow-slate-700 ">
-        <div className="overflow-scroll">
+        <div className="h-full overflow-scroll">
           {messages.map((message, i) => {
             return (
               <div
